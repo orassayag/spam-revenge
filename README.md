45f2859710f08900d4150b2d74548c6d937d040b
Or Assayag <orassayag@gmail.com>
2021-02-28 16:10:40 +0200

    initiate project

---
+++ README.md
@@ -0,0 +1,50 @@
+# Spam Revenge
+
+Built in February 2021. A Node.js + Puppeteer.js application to test some NPM packages to get, validate, and implement different proxis in puppeteer crawling process.
+
+## Getting Started
+
+Clone the application to your computer.
+Steps to view the solution:
+1. Open server application on IDE (I use VSCode).
+2. Terminal: npm i
+3. In terminal: npm start
+
+READ THE FOLLOWING SETTINGS BEFORE YOU BEGIN:
+
+-Instructions & How to use:
+1. Go to the settings.js file and make sure that all settings are fit to needs.
+2. Read the full instructions guide in INSTRUCTIONS.md file.
+3. Run the script by "npm start" and let the magic begin. :)
+
+In terminal: npm start
+
+Happy testing! :)
+
+### Prerequisites
+
+You'll need to install VSCode, Node, and clone the application, run npm i.
+
+## Built With
+
+* [Node.JS](https://nodejs.org/en/) - The web framework used - Server side.
+
+## Contributing
+
+Please read [CONTRIBUTING.md](https://gist.github.com/PurpleBooth/b24679402957c63ec426) for details on our code of conduct, and the process for submitting pull requests to us.
+
+## Versioning
+
+We use [SemVer](http://semver.org/) for versioning. For the versions available, see the [tags on this repository](https://github.com/your/project/tags).
+
+## Author
+
+* **Or Assayag** - *Initial work* - [orassayag](https://github.com/orassayag)
+* Or Assayag <orassayag@gmail.com>
+* GitHub: https://github.com/orassayag
+* StackOverFlow: https://stackoverflow.com/users/4442606/or-assayag?tab=profile
+* LinkedIn: https://il.linkedin.com/in/orassayag
+
+## License
+
+This application has UNLICENSED License.
---
+++ src/index.js
@@ -0,0 +1,144 @@
+const testPuppeteerWithProxy = async () => {
+    const puppeteer = require('puppeteer');
+    const browser = await puppeteer.launch({
+        headless: true,
+        args: ['--proxy-server=socks4://177.154.55.114:51815']
+    });
+    const page = await browser.newPage();
+    const cdpRequestDataRaw = await setupLoggingOfAllNetworkData(page);
+    // Make requests.
+    await page.goto('https://api.ipify.org');
+
+    /*     const response = await page.goto('https://api.ipify.org');
+        const headers = response.headers();
+        setTimeout(() => {
+            console.log(response);
+            //console.log(headers);
+            browser.close();
+        }, 3000); */
+    /* }); */
+
+    // Log captured request data.
+    console.log(JSON.stringify(cdpRequestDataRaw, null, 2));
+    setTimeout(() => {
+        browser.close();
+    }, 3000);
+    // Returns map of request ID to raw CDP request data. This will be populated as requests are made.
+    async function setupLoggingOfAllNetworkData(page1) {
+        const cdpSession = await page1.target().createCDPSession();
+        await cdpSession.send('Network.enable');
+        const cdpRequestDataRaw1 = {};
+        const addCDPRequestDataListener = (eventName) => {
+            cdpSession.on(eventName, request => {
+                cdpRequestDataRaw1[request.requestId] = cdpRequestDataRaw1[request.requestId] || {};
+                Object.assign(cdpRequestDataRaw1[request.requestId], { [eventName]: request });
+            });
+        };
+        addCDPRequestDataListener('Network.requestWillBeSent');
+        addCDPRequestDataListener('Network.requestWillBeSentExtraInfo');
+        addCDPRequestDataListener('Network.responseReceived');
+        addCDPRequestDataListener('Network.responseReceivedExtraInfo');
+        return cdpRequestDataRaw1;
+    }
+};
+
+const getYourPublicIPAddress1 = () => {
+    const http = require('http');
+    var options = {
+        host: 'ipv4bot.whatismyipaddress.com',
+        port: 80,
+        path: '/'
+    };
+    http.get(options, function (res) {
+        console.log('status: ' + res.statusCode);
+
+        res.on('data', function (chunk) {
+            console.log('BODY: ' + chunk);
+        });
+    }).on('error', function (e) {
+        console.log('error: ' + e.message);
+    });
+};
+
+
+const getYourPublicIPAddress2 = () => {
+    var os = require('os');
+    var networkInterfaces = os.networkInterfaces();
+    console.log(networkInterfaces);
+}
+
+const verifyProxy = () => {
+    /*     {
+            ipAddress: '96.82.74.129',
+            port: 34032,
+            anonymityLevel: 'elite',
+            protocols: [ 'socks4' ],
+            country: 'us',
+            source: 'proxyscrape-com'
+          },
+          {
+            ipAddress: '192.111.129.145',
+            port: 16894,
+            anonymityLevel: 'elite',
+            protocols: [ 'socks4' ],
+            country: 'us',
+            source: 'proxyscrape-com'
+          },
+          {
+            ipAddress: '24.172.34.114',
+            port: 60133,
+            anonymityLevel: 'elite',
+            protocols: [ 'socks4' ],
+            country: 'us',
+            source: 'proxyscrape-com'
+          },
+          ... 5 more items
+        ] */
+
+    const ProxyVerifier = require('proxy-verifier');
+    const proxy = {
+        ipAddress: '185.128.136.231',
+        port: 3128,
+        protocol: 'socks4'
+    };
+
+    ProxyVerifier.testAll(proxy, function (error, result) {
+        if (error) {
+            console.log(error);
+            // Some unusual error occurred.
+        } else {
+            // The result object will contain success/error information.
+            console.log(result);
+        }
+    });
+
+};
+
+const getProxiesList = () => {
+    const ProxyLists = require('proxy-lists');
+    ProxyLists.getProxies({
+        // options
+        countries: ['us', 'ca']
+    })
+        .on('data', function (proxies) {
+            // Received some proxies.
+            console.log('got some proxies');
+            console.log(proxies);
+        })
+        .on('error', function (error) {
+            // Some error has occurred.
+            console.log('error!', error);
+        })
+        .once('end', function () {
+            // Done getting proxies.
+            console.log('end!');
+        });
+};
+
+(async () => {
+    await testPuppeteerWithProxy();
+    getYourPublicIPAddress1();
+    getYourPublicIPAddress2();
+    verifyProxy();
+    getProxiesList();
+})();
